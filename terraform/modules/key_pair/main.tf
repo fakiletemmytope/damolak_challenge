31fe5d2bbf42fbf3879d8175a40ce53aa229b427
fakiletemmytope <fakiletemitope@gmail.com>
2026-05-10 21:54:40 +0100

    terraform update

--- terraform/modules/key_pair/main.tf
+++ terraform/modules/key_pair/main.tf
@@ -1,9 +1,7 @@
-# Generate the key pair
 resource "tls_private_key" "main" {
   algorithm = "ED25519"
 }
 
-# Register the public key with AWS
 resource "aws_key_pair" "main" {
   key_name   = var.key_pair_name
   public_key = tls_private_key.main.public_key_openssh
@@ -12,5 +10,5 @@ resource "aws_key_pair" "main" {
 resource "aws_ssm_parameter" "private_key" {
   name  = "/damolak/ec2-private-key"
   type  = "SecureString"
-  value = tls_private_key.main.private_key_pem
+  value = tls_private_key.main.private_key_openssh
 }
